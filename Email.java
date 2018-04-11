6b2ea11ddbfdeb24ca39ec8a0efbe22e5f88bfe0
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2018-04-11 12:41:55 -0500

    Update Email.java

--- Email.java
+++ Email.java
@@ -1,2 +1,18 @@
 
-public Class
+public class Email 
+{
+  private String username, domain, extension;
+  
+  public Email()
+  {
+    username = null;
+    domain = null;
+    extension = null;
+  }
+  public Email(String username_, String domain_,String extension_)
+  {
+    username = username_;
+    domain = domain_;
+    extension = extension_;
+  }
+  
