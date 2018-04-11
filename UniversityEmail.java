af0d843d08fddaab719288528e338a21f8a37593
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2018-04-11 13:31:50 -0500

    Update UniversityEmail.java

--- UniversityEmail.java
+++ UniversityEmail.java
@@ -1,1 +1,21 @@
 
+public class UniversityEmail extends Email 
+{
+    private String department;
+    private int code;
+    
+    public UniversityEmail()
+    {
+    department = null;
+    code = 0;
+    }
+    
+    public UniversityEmail(String username_, String domain_, String extension_,String department_,int code_)
+    {
+        super(username_,domain_,extension_);
+        department = department_;
+        code = code_;
+    }   
+    public void getCode()
+    {
+        
