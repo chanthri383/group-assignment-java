276b04bcc1382eb831dd8bc0e90514e15edbd11a
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2018-04-15 17:18:26 -0500

    Update UniversityEmail.java

--- UniversityEmail.java
+++ UniversityEmail.java
@@ -36,7 +36,7 @@ public class UniversityEmail extends Email
     {
         String text = "";
         
-        text = super.toString().substring(0,indexOf('@')) + department + "." ;
+        text = super.toString().substring(0,indexOf('@')) + department + "." + super.toString().substring(indexOf('@' + 1) ;
         
         return text;
 }
